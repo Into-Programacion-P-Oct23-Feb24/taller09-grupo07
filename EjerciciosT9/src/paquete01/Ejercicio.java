0c369e30af3dafe76d018f67829c2bb1f867e733
Josuee16 <josuexpardoch@gmail.com>
2023-12-14 17:16:59 -0500

    Taller Grupal

--- EjerciciosT9/src/paquete01/Ejercicio.java
+++ EjerciciosT9/src/paquete01/Ejercicio.java
@@ -4,10 +4,57 @@
  */
 package paquete01;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejercicio {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int n;
+        int contador = 0;
+        String marca;
+        String resultado = "";
+        System.out.println("Ingrese el numero de elementos que desea");
+        
+        // se pide el numero de elementos para el arreglo
+        
+        n = entrada.nextInt();
+        
+        // se guarda el numero de elementos del arreglo
+        
+        entrada.nextLine();
+        
+        // limpieza buffer
+        
+        String[] marcas = new String[n];
+            
+        while (contador < n) {
+            System.out.println("Ingrese la marca de un automovil");
+            marca = entrada.nextLine();
+            String inicial = marca.substring(0, 1);
+            if (inicial.equals("A") || inicial.equals("C")
+                    || inicial.equals("T")) {
+                System.out.println("ERROR, datos no permitidos");
+            } else {
+                marcas[contador] = marca;
+                contador = contador + 1;
+            }
+            /* El while se utiliza para poder generar la restriccion donde se debe
+            de re-hacer la pregunta sin aumentar el contador ni guardar la
+            informacion en el arreglo, se utiliza un condicional para mayor
+            facilidad
+            */
+        }
+        for (int i = 0; i < marcas.length; i++) {
+            resultado = String.format("%s%s\n", resultado, marcas[i]);
+        }
+        System.out.printf("%s", resultado);
+    }
+
 }
--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -16,8 +16,20 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int suma = 0;
+        int promedio;
+        String resultado = "";
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        
+        for (int i = 0; i < arreglo.length;i++){
+            suma = suma + arreglo[i];
+            promedio = suma / 13;
+            if (arreglo[i] > promedio){
+                resultado = String.format("%s%d"
+                        ,resultado,arreglo[i]);
+                             
+            }
+        }
+        System.out.printf("%s",resultado);
     }
 
 }
