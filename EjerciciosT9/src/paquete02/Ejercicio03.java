359451f212e19e82c8a7c411655f30ed2292fdf2
Josuee16 <josuexpardoch@gmail.com>
2023-12-15 13:34:49 -0500

    Trabajo Acabado

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -16,24 +16,27 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int suma = 0;
-        int promedio;
-        String resultado = "";
+        double suma = 0;
+        int arriba = 0;
+        int abajo = 0;
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
         for (int i = 0; i < arreglo.length; i++) {
             suma = suma + arreglo[i];
-            promedio = suma / 13;
-            if (arreglo[i] > promedio) {
-                resultado = String.format("%s%d",
-                         resultado, arreglo[i]);
+
+        }
+        double promedio = suma / arreglo.length;
+        for (int j = 0; j < arreglo.length; j++) {
+            if (arreglo[j] > promedio) {
+                arriba++;
             } else {
-                if (arreglo[i] < promedio) {
-                    resultado = String.format("%s%d",
-                             resultado, arreglo[i]);
+                if (arreglo[j] < arreglo.length) {
+                    abajo++;
                 }
             }
         }
-        System.out.printf("%s", resultado);
+        System.out.printf("Promedio: %.2f\nCantidad de Elementos por "
+                + "arriba del promedio: %d\nElementos por debajo del promedio: "
+                + "%d\n",promedio,arriba,abajo);
     }
 
 }
